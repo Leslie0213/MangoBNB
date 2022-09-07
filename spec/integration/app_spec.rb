29d85bfc115e423417e4a9ac29ac9aedc0f529f6
Rex Gonsalves <rex_13@rediffmail.com>
2022-09-07 14:17:04 +0100

    Create space functionality added

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -29,4 +29,27 @@ describe Application do
         expect(response.body).to include('razbugoi@gmail.com')
       end
     end
+
+    context "GET /spaces" do
+        it 'displays all the spaces post successful user login' do
+         
+          response = get('/spaces')
+    
+          expect(response.status).to eq(200)
+          expect(response.body).to include("<h1>Spaces Listed</h1>")
+        end
+      end
+
+      context 'POST /spaces' do
+        it 'creates a new space in the database' do
+          response = post('/spaces', name: 'Holiday Cottages', description: 'Nice cozy cottages', price: '120', user_id: '1')
+    
+          expect(response.status).to eq 302
+          expect(response.body).to eq ""
+    
+          response = get('/spaces')
+    
+          expect(response.body).to include('Holiday Cottages')
+        end
+      end
 end
