933f994d1fc6f3b6ab32b3ac5a03532edc785ca7
Razvan Bugoi <razbugoi@gmail.com>
2022-09-08 17:13:51 +0100

    added more tests

--- spec/integration/app_spec.rb
+++ spec/integration/app_spec.rb
@@ -54,4 +54,75 @@ describe Application do
           expect(response.body).to include('Holiday Cottages')
         end
       end
+
+      context 'GET /signup_error' do
+        it 'returns an error if the username already exists in the database' do
+          response = get('/signup_error')
+    
+          expect(response.status).to eq 200
+          expect(response.body).to include('Please use different username')
+        end
+      end
+
+      context 'GET /confirmation' do
+        it 'returns a confirmation message if the account has been created' do
+          response = get('/confirmation')
+    
+          expect(response.status).to eq 200
+          expect(response.body).to include('<h1>Account created successfully! You can now <a href="/login">log in</a></h1>')
+        end
+      end
+
+      context 'GET /login' do
+        it 'returns the log in page' do
+          response = get('/login')
+    
+          expect(response.status).to eq 200
+          expect(response.body).to include('<h1>Log in to MangoBnB</h1>')
+        end
+      end
+
+      context 'GET /spaces/new' do
+        it 'returns the create space page where user can add a new space to the database' do
+          response = get('/spaces/new')
+    
+          expect(response.status).to eq 200
+          expect(response.body).to include('<button type="submit" class="btn btn-primary">Create Space</button>')
+        end
+      end
+
+      context 'POST /login/validation' do
+        it 'checks if the user credentials match the data in the database' do
+          response = post('/login/validation', email: 'raz@razbugoi.co.uk', pass_word: 'rxcvx21@!*&')
+    
+          expect(response.status).to eq 302
+          expect(response.body).to eq ""
+    
+          response = get('/spaces')
+    
+          expect(response.body).to include('The Old Bakery')
+        end
+      end
+
+      context 'POST /login/validation' do
+        it 'returns an error page if the credentials are incorrect' do
+          response = post('/login/validation', email: 'razasd231@razbugoi.co.uk', pass_word: 'rxcvxvcbvbx21@!*&')
+    
+          expect(response.status).to eq 302
+          expect(response.body).to eq ""
+    
+          response = get('/authentication/error')
+    
+          expect(response.body).to include('<h1>UserID or Password incorrect <a href="/login">Please try again</a> </h1>')
+        end
+      end
+
+      context 'GET /authentication/error' do
+        it 'returns an error message if the authentication fails' do
+          response = get('/authentication/error')
+    
+          expect(response.status).to eq 200
+          expect(response.body).to include('<h1>UserID or Password incorrect <a href="/login">Please try again</a> </h1>')
+        end
+      end
 end
